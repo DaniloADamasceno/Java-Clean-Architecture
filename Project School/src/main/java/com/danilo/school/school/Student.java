01f6e94a7eadff2eb68706cc1c4efd889b7bf555
DANILO ALMEIDA DAMASCENO <danilo.engauto@gmail.com>
2024-01-04 16:23:00 -0300

    :tada: add classes email, cpf e aluno

---
+++ Project School/src/main/java/com/danilo/school/school/Student.java
@@ -0,0 +1,14 @@
+package com.danilo.school.school;
+
+public class Student {
+
+    private String name;
+    private CPF cpf;
+    private Email email;
+    private String phone;
+    private String registration;
+
+
+
+
+}
