5e68b2ae87a295b6ca3b25f4dbf16543ae2604fc
DANILO ALMEIDA DAMASCENO <danilo.engauto@gmail.com>
2024-01-04 16:23:00 -0300

    :sparkles: alguns testes unitarios e aluno e suas classes

---
+++ Project School/src/test/java/com/danilo/school/school/EmailTest.java
@@ -0,0 +1,20 @@
+package com.danilo.school.school;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailTest {
+
+    @Test
+    void shouldntCreateEmailsWithInvalidAddresses() { // Não deve criar emails com endereços inválidos
+        assertThrows(IllegalArgumentException.class, () -> new Email(null)); // se eu passar null, deve lançar uma exceção
+        assertThrows(IllegalArgumentException.class, () -> new Email("")); // se eu passar uma string vazia, deve lançar uma exceção
+        assertThrows(IllegalArgumentException.class, () -> new Email("emailinvalido")); // se eu passar um email inválido, deve lançar uma exceção
+        assertThrows(IllegalArgumentException.class, () -> new Email("emailinvalido@")); // se eu passar um email inválido, deve lançar uma exceção
+        assertThrows(IllegalArgumentException.class, () -> new Email("emailinvalido.com")); // se eu passar um email inválido, deve lançar uma exceção
+
+    }
+
+
+}
